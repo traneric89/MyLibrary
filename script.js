7707ed7fcefd6dfcf63a9e70fff8096162b443fb
Eric <traneric89@gmail.com>
2022-01-06 00:06:32 -0500

    finished form styling

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const addButton = document.getElementById("add");
+
+addButton.addEventListener("click", () => {
+  console.log(myLibrary);
+});
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -10,20 +16,19 @@ function Book(title, author, pages, read) {
 const addBookToLibrary = (title, author, pages, read) => {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  console.log(newBook);
-  console.log(myLibrary);
 };
 
 addBookToLibrary("Harry Potter", "J.K. Rowling", "1024", "Yes");
 addBookToLibrary("Percy Jackson", "Rick Riordan", "675", "No");
 
+const readStatus = () => {
+  console.log("button working");
+};
+
 myLibrary.forEach((book) => {
   let newDiv = document.createElement("div");
   newDiv.className = "book";
   newDiv.setAttribute("id", "book");
-
-  let checkboxDiv = document.createElement("div");
-  checkboxDiv.className = "checkboxDiv";
 
   let library = document.getElementById("books");
   library.appendChild(newDiv);
@@ -34,30 +39,28 @@ myLibrary.forEach((book) => {
   let author = document.createElement("h2");
   let pages = document.createElement("h2");
 
-  let read = document.createElement("input");
-  read.type = "checkbox";
-  read.id = "readStatus" + index;
-  read.className = "checkbox";
-
-  let label = document.createElement("label");
-  label.htmlFor = read.id;
-  label.className = "checkbox";
+  let read = document.createElement("button");
+  read.className = "read";
+  //read.onclick = readStatus;
+  read.addEventListener = ("click", () => readStatus());
 
   if (myLibrary[index].read === "Yes") {
-    label.innerHTML = "Yes";
+    read.innerHTML = "Read";
   } else {
-    label.innerHTML = "No";
+    read.innerHTML = "Not Read";
+    read.style.backgroundColor = "#f44336";
   }
 
   newDiv.appendChild(title);
   newDiv.appendChild(author);
   newDiv.appendChild(pages);
-  newDiv.appendChild(checkboxDiv);
-  checkboxDiv.appendChild(read);
-  checkboxDiv.appendChild(label);
+  newDiv.appendChild(read);
 
   title.innerHTML = myLibrary[index].title;
   author.innerHTML = myLibrary[index].author;
   pages.innerHTML = myLibrary[index].pages;
-  read.innerHTML = myLibrary[index].read;
 });
+
+// const readStatus = () => {
+//   console.log("button working");
+// };
